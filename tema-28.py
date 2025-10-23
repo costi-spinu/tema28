7592c444acbc41b319036c69cc20d86aa1dccfe9
Spinu Costelus <costyspinu@gmail.com>
2025-10-23 20:22:54 +0300

    update

---
+++ tema-28.py
@@ -0,0 +1,6 @@
+# Task
+# Clone a repository.
+# Create a new branch in the repository called "updates" and switch to it.
+# Make changes of all types (add, modify, delete) to the new branch of the repository.
+# Publish the cloned repository on your own account.
+# Add a link to this repository to your homework report.
